276a80b0f4875755f641f111b25fb991b2764058
Wenwei Huang <dut.hww@gmail.com>
2017-06-16 16:56:59 +0800

    Set output directory

--- shkey_frame_data.py
+++ shkey_frame_data.py
@@ -2,14 +2,17 @@
 #
 # This script should be run in blender.
 # It fetches the face data of 200 frames for a specific animation.
-#
 
 import roscom
 import bpy
 from collections import defaultdict
 import time
+import os
 
 frames = defaultdict(list)
+output_dir = os.path.expanduser('~/shapekey_data')
+if not os.path.isdir(output_dir):
+    os.makedirs(output_dir)
 
 for action in bpy.data.actions:
     if not action.name.startswith('EMO-'):
@@ -17,20 +20,19 @@ for action in bpy.data.actions:
     bpy.context.object.animation_data.action = action
     name = action.name[4:]
 
-    frame_size = 200
+    frame_size = bpy.context.scene.frame_end+1
     for i in range(frame_size):
-        bpy.context.scene.frame_set(i+1)
+        bpy.context.scene.frame_set(i)
         time.sleep(0.01)
         data=roscom.api.getFaceData()
         print("frame", i)
         for k, v in data.items():
             frames[k].append(v)
 
-    with open('{}.csv'.format(name), 'w') as f:
+    with open(os.path.join(output_dir, '{}.csv'.format(name)), 'w') as f:
         header = sorted(frames.keys())
         f.write('%s\n' % ','.join(header))
         for i in range(frame_size):
             values = [frames[k][i] for k in header]
             f.write('%s\n' % ','.join(map(str, values)))
         print('write done')
-
